806d7337411b057d46869a175636d34fe3420e51
Grégory Marini <greg.marini@hotmail.fr>
2023-06-30 19:23:12 +0200

    work on star functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import StarRating from './components/StarRating';
+import StarRating from './components/startRating/StarRating';
 // import './index.css';
 // import App from './App';
 
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating maxRating={10} />
   </React.StrictMode>
 );
