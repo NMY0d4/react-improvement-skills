33608cf07b2870a3bfb679289607679d6fd29486
Grégory Marini <greg.marini@hotmail.fr>
2023-07-01 10:57:07 +0200

    improve starRating component, and add options

--- src/components/startRating/StarRating.js
+++ src/components/startRating/StarRating.js
@@ -11,26 +11,43 @@ const starContainerStyle = {
   display: 'flex',
 };
 
-const textStyle = {
-  lineHeight: '0',
-  margin: '0',
-};
-
-const StarRating = ({ maxRating = 5 }) => {
+const StarRating = ({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  messages = [],
+}) => {
   const [rating, setRating] = useState(0);
-  const full = false;
+  const [tempRating, setTempRating] = useState(0);
+
+  const textStyle = {
+    lineHeight: '0',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: +maxRating }, (_, i) => (
           <Star
             key={i}
             onRate={() => setRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ''}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ''}
+      </p>
     </div>
   );
 };
